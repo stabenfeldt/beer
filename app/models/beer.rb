dc19c95b65a37f380d0e6de25f1657469b6e4c9a
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-06-02 16:39:38 +0200

    Added Beers

---
+++ app/models/beer.rb
@@ -0,0 +1,30 @@
+class Beer
+  include MotionModel::Model
+  include MotionModel::ArrayModelAdapter
+  
+  columns location: :hash,
+          name:     :string,
+          url:      :string,
+          sun_from: :date,
+          sun_to:   :date
+  
+
+  def title;      name;       end
+  def url;        url;        end
+
+  def coordinate
+    loc           = CLLocationCoordinate2D.new
+    loc.latitude  = location[:latitude]
+    loc.longitude = location[:longitude]
+    loc
+  end
+
+  def has_sun_now?
+    (sun_from..sun_to).cover? Time.now
+  end
+
+  def self.places_with_sun_now(beers)
+    beers.select { |b| b.has_sun_now? }
+  end
+
+end
